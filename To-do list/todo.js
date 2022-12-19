976aa2c9ad31d983565db5941b036657374039a4
Homazd <homa.zohdi@hotmail.com>
2022-12-19 13:59:41 +0330

    complete shapeAssertion function

--- To-do list/todo.js
+++ To-do list/todo.js
@@ -92,6 +92,25 @@ function createStore(reducer, initialState){
     }
 }
 
+function shapeAssertionReducers(reducers){
+  Object.entries(reducers).forEach(([reducerKey, reducer]) => {
+    const action = {type: "@INIT", target : "reducerKey"}
+    const state = reducer(undefined, action);
+
+    if(typeof state === "undefined"){
+      throw new Error(`Reducer for key ${reducerKey} return undefined for the action ${JSON.stringify(action)}`)
+    }
+
+    const randomActionType = Math.random().toString(16).slice(2);
+    const secondAction = {type: randomActionType, target: reducerKey};
+    const secondState = reducer(undefined, secondAction);
+
+    if(typeof secondState === "undefined"){
+      throw new Error(`Reducer for the key ${reducerKey} returns undefined for the action ${JSON.stringify(secondAction)}`);
+    }
+  })
+}
+
 function kindOf(inp){
     return Object.prototype.toString.call(inp).slice(8, -1).toLowerCase();
 }
