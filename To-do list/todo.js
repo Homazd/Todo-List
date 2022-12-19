890c6fd964f8f280be569627c0099fc4fb3b4e08
Homazd <homa.zohdi@hotmail.com>
2022-12-19 14:21:43 +0330

    Complete Combine reducers function

--- To-do list/todo.js
+++ To-do list/todo.js
@@ -111,6 +111,68 @@ function shapeAssertionReducers(reducers){
   })
 }
 
+function combineReducers(reducers){
+  let finalReducers = {};
+
+  for (const reducerKey in reducers){
+    const reducer = reducers[reducerKey]
+    if(isFunction(reducer)){
+      finalReducers[reducerKey] = reducer;
+    }
+
+  }
+  let shapeError;
+
+  try {
+    shapeAssertionReducers(finalReducers);
+  } catch (e) {
+    shapeError = e;
+  }
+
+  return (state = {}, action) => {
+    if(shapeError){
+      throw shapeError;
+    }
+    let hasChanged = false;
+    const nextState = state;
+    if(action.type === "@INIT" || action.target === "*"){
+    for (const reducerKey in finalReducers){
+      const reducer = finalReducers[reducerKey];
+      const reducerState = state[reducerKey] || undefined;
+      delete action.target;
+      const newReducerState = reducer(reducerState, action);
+
+      if(typeof newReducerState === "undefined"){
+        throw new Error(`Reducer for the key ${reducerKey} returns undefined for the action's type ${action.type}`)
+      }
+      hasChanged = hasChanged || newReducerState !== reducerState;
+      nextState[reducerKey] = newReducerState;
+    }
+  } else{
+    const reducerKey = action.target;
+    if(!(reducerKey in finalReducers)){
+      throw new Error(`Target ${reducerKey} can't be found in finalReducers.`)
+    }
+    const reducer = finalReducers[reducerKey];
+      const reducerState = state[reducerKey] || undefined;
+      delete action.target;
+      const newReducerState = reducer(reducerState, action);
+
+      if (typeof newReducerState === "undefined") {
+        throw new Error(
+          `Reducer ${reducerKey} returns undefined for action's type ${action.type}.`
+        );
+      }
+
+      hasChanged = reducerState !== newReducerState;
+
+      if (hasChanged) nextState[reducerKey] = newReducerState;
+  }
+return hasChanged ? nextState : state;
+};
+}
+
+
 function kindOf(inp){
     return Object.prototype.toString.call(inp).slice(8, -1).toLowerCase();
 }
